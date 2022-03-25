a040179769ea660b053b1b5a2b11d82d745e029a
Lauren Colvin <lauren@Laurens-MBP-2.lan>
2022-03-25 14:09:27 -0700

    adding user component

---
+++ src/Components/User/User.js
@@ -0,0 +1,10 @@
+const User = () => {
+    return (
+        <div className='user-page'>
+            <h1>Please login to see your wishlist and wardrobe</h1>
+            <button>Start Dreaming</button>
+        </div>
+    )
+}
+
+export default User;
