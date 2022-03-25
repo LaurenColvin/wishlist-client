f3960813cce35dbfc95adcd640a82deab3c2b282
Lauren Colvin <lauren@Laurens-MBP-2.lan>
2022-03-25 13:58:39 -0700

    adding homepage component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className='home-page'>
+            <h1>Shop Smarter, Not Harder.</h1>
+            <button>Start Dreaming</button>
+        </div>
+    )
+}
+
+export default Home;
