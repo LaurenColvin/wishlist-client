d184060f71b9c260f20f501593e0a353f8259985
Lauren Colvin <lauren@Laurens-MBP-2.lan>
2022-03-25 14:09:51 -0700

    importing all components and adding routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Header from './Components/Header/Header';
+import ScrollTop from './Components/ScrollTop/ScrollTop';
+import Home from './Components/Home/Home';
+import Category from './Components/Category/Category';
+import About from './Components/About/About';
+import User from './Components/User/User';
+import Cart from './Components/Cart/Cart';
+import Footer from './Components/Footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main>
+        <ScrollTop>
+          <Routes>
+            <Route path="/" element={  <Home/> }/>
+            <Route path="/category" element={  <Category/> }/>
+            <Route path="/about" element={  <About/> }/>
+            <Route path="/user" element={  <User/> }/>
+            <Route path="/cart" element={  <Cart/> }/>
+          </Routes>
+        </ScrollTop>
+      </main>
+      <Footer/>
     </div>
   );
 }
