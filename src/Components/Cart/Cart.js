12e71c1cf785709528e2e6fef15a80c8e98b20f7
Lauren Colvin <lauren@Laurens-MBP-2.lan>
2022-03-25 14:09:01 -0700

    adding Cart component

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,10 @@
+const Cart = () => {
+    return (
+        <div className='cart-page'>
+            <h1>You found your dream piece!</h1>
+            <button>Purchased</button>
+        </div>
+    )
+}
+
+export default Cart;
