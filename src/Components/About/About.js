af54687db60fea195ffc6ab54dd42200d60c3a64
Lauren Colvin <lauren@Laurens-MBP-2.lan>
2022-03-25 14:08:44 -0700

    adding About component

---
+++ src/Components/About/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className='about-page'>
+            <h1>About Wishlist</h1>
+            <p>Wishlist is an online shopping app to help you plan your purchases. So much better than having a million tabs open and carts filled with items you can’t remember at 20 different stores. Put all of your ideal items in one place to better plan and save up for your dream wardrobe! This app will also help with budgeting and making sure you are making the best purchase at the best price.</p>
+        </div>
+    )
+}
+
+export default About;
