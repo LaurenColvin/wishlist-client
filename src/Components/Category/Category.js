95500fe5cf06e7430401c4a5af65c388b0bf3f54
Lauren Colvin <lauren@Laurens-MBP-2.lan>
2022-03-25 13:58:56 -0700

    adding category component to add items

---
+++ src/Components/Category/Category.js
@@ -0,0 +1,10 @@
+const Category = () => {
+    return (
+        <div className='home-page'>
+            <h1>What have you been dreaming about?</h1>
+            <button>Add Items</button>
+        </div>
+    )
+}
+
+export default Category;
