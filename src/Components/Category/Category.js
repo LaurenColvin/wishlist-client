8449a2ed9ba42b70ad6c87d4c732af71a6c53f12
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-28 14:37:09 -0700

    adding form for new category

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,8 +1,54 @@
+import { useState } from "react";
+
 const Category = () => {
+
+    const [showForm, setShowForm] = useState(false);
+    const [category, setCategory] = useState("");
+    const [categoryList, setCategoryList] = useState([]);
+
+    const handleShow = () => setShowForm(true);
+
+    const categoryHandleChange = (event) => {
+        event.preventDefault();
+        setCategory(event.target.value);
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setShowForm(false);
+        const categoriesCopy = [...categoryList];
+        categoriesCopy.push(category);
+        setCategoryList(categoriesCopy)
+        setCategory("");
+        console.log(category)
+      };
+
+    const list = categoryList.map((category) => {
+        return (
+            <div className='category-box'>
+                <h2>{category}</h2>
+            </div>
+        )
+    })
+
+
     return (
         <div className='category-page'>
-            <h1>What have you been dreaming about?</h1>
-            <button>Add Items</button>
+            <div className='welcome-box'>
+                <h1>What have you been dreaming about?</h1>
+            </div>
+            <div className='categories'>
+                <button onClick={handleShow}>Create New Category</button>
+                {showForm == true? (
+                <form className='new-category-form' onSubmit={handleSubmit}>
+                  <input onChange={categoryHandleChange} name="category" placeholder="Shopping for..." value={category} type="text" required/>
+                <input className="submit-button" type="submit"></input>
+              </form>
+              ):(<div></div>)}
+                {categoryList != undefined ? (
+                <div className="category-list">{list}</div>
+              ):(<div></div>)}
+            </div>
         </div>
     )
 }
