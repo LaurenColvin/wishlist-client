59319846c544f53b5a8e2be087475c7918938015
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-28 15:53:39 -0700

    importing add icon

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import Cart from './Components/Cart/Cart';
 import Footer from './Components/Footer/Footer';
 
 
+////////////////////// FONTAWESOME ///////////////////
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCirclePlus} from '@fortawesome/free-solid-svg-icons'
+
+let addIcon = <FontAwesomeIcon className="add-icon" icon={faCirclePlus} size="2x" style={{color:"#FA5272"}}/>
+
+
 function App() {
   return (
     <div className="App">
@@ -18,7 +25,7 @@ function App() {
         <ScrollTop>
           <Routes>
             <Route path="/" element={  <Home/> }/>
-            <Route path="/category" element={  <Category/> }/>
+            <Route path="/category" element={  <Category addIcon={addIcon}/> }/>
             <Route path="/about" element={  <About/> }/>
             <Route path="/user" element={  <User/> }/>
             <Route path="/cart" element={  <Cart/> }/>
