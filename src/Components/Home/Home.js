fa01f9059a5ab152a5878744e85c398bbfd74b84
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-28 21:18:55 -0700

    adding login modal to homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,78 @@
 import {Link} from 'react-router-dom'
+import { useState } from "react";
+
 
 const Home = () => {
+
+    /* MODAL SHOW */
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShow = (event) => {
+        event.preventDefault();
+        setShowModal(true);
+    };
+
+    const handleClose = (event) => {
+        event.preventDefault();
+        setShowModal(false);
+    };
+
+    /* FORM USESTATES AND HANDLE CHANGES */
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [budget, setBudget] = useState("");
+    const [currentUser, setCurrentUser] = useState("");
+    const [blank, setBlank] = useState("");
+
+    const firstNameHandleChange = (event) => {
+        event.preventDefault();
+        setFirstName(event.target.value);
+      };
+
+    const lastNameHandleChange = (event) => {
+        event.preventDefault();
+        setLastName(event.target.value);
+      };
+
+    const emailHandleChange = (event) => {
+        event.preventDefault();
+        setEmail(event.target.value);
+      };
+
+    const budgetHandleChange = (event) => {
+        event.preventDefault();
+        setBudget(event.target.value);
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setBudget("");
+        console.log({firstName});
+      };
+
+    const userHandleChange = (event) => {
+        event.preventDefault();
+        setBlank(event.target.value);
+        setCurrentUser(event.target.value);
+    }
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        setBlank("");
+        console.log(currentUser);
+    }
+
     return (
         <div className='home-page'>
             <div className='hero-image'>
                 <h1>Shop Smarter,<br/> Not Harder.</h1>
-                <Link className='button' to='/category'>Start Dreaming</Link>
+                <button className='button' onClick={handleShow}>Start Dreaming</button>
             </div>
             <div className='trending'>
                 <h2>Popular Categories</h2>
@@ -46,6 +113,33 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            { showModal == true ? (
+                <div className='modal'>
+                    <h2>CREATE NEW USER</h2>
+                    <form className='new-user-form' onSubmit={handleSubmit}>
+                        <input onChange={firstNameHandleChange} className="text-box" name="first-name" placeholder="First Name" value={firstName} type="text" required/>
+                        <br/>
+                        <input onChange={lastNameHandleChange} className="text-box" name="last-name" placeholder="Last Name" value={lastName} type="text" required/>
+                        <br/>
+                        <input onChange={emailHandleChange} className="text-box" name="last-name" placeholder="Email" value={email} type="text" required/>
+                        <br/>
+                        <label>How much do you want to spend each month?</label>
+                        <br/>
+                        <input onChange={budgetHandleChange} className="text-box" name="last-name" placeholder="Budget" value={budget} type="number" required/>
+                        <br/>
+                        <input className="submit-button" type="submit" value="Submit"></input>
+                    </form>
+                    <h2>LOGIN IN</h2>
+                    <form className='new-user-form' onSubmit={handleLogin}>
+                        <input onChange={userHandleChange} className="text-box" name="email" placeholder="Email" value={blank} type="text" required/>
+                        <br/>
+                        <input className="submit-button" type="submit" value="LOGIN"></input>
+                    </form>
+                    <button onClick={handleClose}>Exit</button>
+                </div>
+            ):(
+                <div></div>
+            )}
         </div>
     )
 }
