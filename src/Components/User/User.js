32563dc153f18ea5aa3344dc14f246015210a266
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-29 11:33:24 -0700

    adding header for page

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,17 +1,60 @@
 import { useState, useEffect } from "react";
+import Login from "../Login/Login";
 
 const User = (props) => {
 
-    useEffect(() => {
+     ////////////////// LOGIN MODAL //////////////////
+
+     const [showModal, setShowModal] = useState(false);
+
+     const handleShow = (event) => {
+         event.preventDefault();
+         setShowModal(true);
+     };
+ 
+     const handleClose = () => {
+         setShowModal(false);
+     };
+
+    //////////////// FETCH USER DATA ///////////////////
+
+    const [userData, setUserData] = useState({})
+
+    const fetchData = () => {
         fetch(props.urlBase + "/user/" + props.currentUser )
           .then((response) => response.json())
-          .then((data) => console.log(data.user));
+          .then((data) => setUserData(data.user));
+    }
+
+    useEffect(() => {
+        fetchData()
+        console.log(props.currentUser)
       }, []);
+
+    useEffect(() => {
+        fetchData()
+    }, [showModal]);
 
     return (
         <div className='user-page'>
-            <h1>Please login to see your wishlist and wardrobe</h1>
-            <button>Start Dreaming</button>
+            <div className='user-box'>
+                <h1>Shop Smarter, Not Harder!</h1>
+            </div>
+            { props.currentUser == "" ? (
+                <div className='login-container'>
+                    <button onClick={handleShow}>Login</button>
+                </div>
+            ):(
+                <div>
+                    <h1>Welcome {userData.firstName}</h1>
+                    <button onClick={handleShow}>Switch User</button>
+                </div>
+            )}
+            { showModal == true ? (
+                <Login handleClose={handleClose} urlBase={props.urlBase} currentUser={props.currentUser} setCurrentUser={props.setCurrentUser}/>
+            ):(
+                <div></div>
+            )}
         </div>
     )
 }
