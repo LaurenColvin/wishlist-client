ef292e3096b4d13209dbf5fefb2f9f0ca847a230
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-29 11:52:19 -0700

    adding container and header for About page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,12 @@
 const About = () => {
     return (
         <div className='about-page'>
-            <h1>About Wishlist</h1>
-            <p>Wishlist is an online shopping app to help you plan your purchases. So much better than having a million tabs open and carts filled with items you can’t remember at 20 different stores. Put all of your ideal items in one place to better plan and save up for your dream wardrobe! This app will also help with budgeting and making sure you are making the best purchase at the best price.</p>
+            <div className='about-box'>
+                <h1>About Wishlist</h1>
+            </div>
+            <div className='about-text'>
+                <p><span>Wishlist is an online shopping app to help you plan your purchases.</span> <br/> <br/>So much better than having a million tabs open and carts filled with items you can’t remember at 20 different stores. Put all of your ideal items in one place to better plan and save up for your dream wardrobe! This app will also help with budgeting and making sure you are making the best purchase at the best price.</p>
+            </div>
         </div>
     )
 }
