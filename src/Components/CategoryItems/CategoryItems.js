bfd6084f810b7dec5749eec3fdd65ae2a14e2754
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-29 15:56:19 -0700

    passing through items list prop

--- src/Components/CategoryItems/CategoryItems.js
+++ src/Components/CategoryItems/CategoryItems.js
@@ -1,9 +1,26 @@
 import { useState, useEffect } from 'react'
 
+import AddItems from '../AddItems/AddItems'
+
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCirclePlus} from '@fortawesome/free-solid-svg-icons'
 
 const CategoryItems = (props) => {
+
+    ////////////////// ADD ITEMS MODAL //////////////////
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShow = (event) => {
+        event.preventDefault();
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
+    ////////////////// SET ITEMS LIST ////////////////////
 
     const [categoryItems, setCategoryItems] = useState([])
 
@@ -26,10 +43,16 @@ const CategoryItems = (props) => {
     return (
         <div className='category-box'>
             <h2>{props.category}</h2>
-            <FontAwesomeIcon className="add-icon" icon={faCirclePlus} size="2x" style={{color:"#FA5272"}}/>
+            <FontAwesomeIcon onClick={handleShow} className="add-icon" icon={faCirclePlus} size="2x" style={{color:"#FA5272"}}/>
             {categoryItems.length == 0 ? (
               <div></div>
             ):(<div className="category-items">{list}</div>)}
+            <button>Compare Items</button>
+            { showModal == true ? (
+                <AddItems handleClose={handleClose} urlBase={props.urlBase} currentUser={props.currentUser} category={props.category} setItems={props.setItems} items={props.items} />
+            ):(
+                <div></div>
+            )}
         </div>
     )
 }
