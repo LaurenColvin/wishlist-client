0041043e14f7ef61f6e246cfbd526082391ec831
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-29 08:49:30 -0700

    moving login modal and adding login section to Home Page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,9 @@
 import {Link} from 'react-router-dom'
 import { useState } from "react";
+import Login from '../Login/Login';
 
 
-const Home = () => {
+const Home = (props) => {
 
     /* MODAL SHOW */
 
@@ -18,55 +19,6 @@ const Home = () => {
         setShowModal(false);
     };
 
-    /* FORM USESTATES AND HANDLE CHANGES */
-
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [budget, setBudget] = useState("");
-    const [currentUser, setCurrentUser] = useState("");
-    const [blank, setBlank] = useState("");
-
-    const firstNameHandleChange = (event) => {
-        event.preventDefault();
-        setFirstName(event.target.value);
-      };
-
-    const lastNameHandleChange = (event) => {
-        event.preventDefault();
-        setLastName(event.target.value);
-      };
-
-    const emailHandleChange = (event) => {
-        event.preventDefault();
-        setEmail(event.target.value);
-      };
-
-    const budgetHandleChange = (event) => {
-        event.preventDefault();
-        setBudget(event.target.value);
-      };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setFirstName("");
-        setLastName("");
-        setEmail("");
-        setBudget("");
-        console.log({firstName});
-      };
-
-    const userHandleChange = (event) => {
-        event.preventDefault();
-        setBlank(event.target.value);
-        setCurrentUser(event.target.value);
-    }
-
-    const handleLogin = (event) => {
-        event.preventDefault();
-        setBlank("");
-        console.log(currentUser);
-    }
 
     return (
         <div className='home-page'>
@@ -113,30 +65,12 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <div className='user-login'>
+                <h2>Ready to get started?</h2>
+                <button className='button' onClick={handleShow}>User Login</button>
+            </div>
             { showModal == true ? (
-                <div className='modal'>
-                    <h2>CREATE NEW USER</h2>
-                    <form className='new-user-form' onSubmit={handleSubmit}>
-                        <input onChange={firstNameHandleChange} className="text-box" name="first-name" placeholder="First Name" value={firstName} type="text" required/>
-                        <br/>
-                        <input onChange={lastNameHandleChange} className="text-box" name="last-name" placeholder="Last Name" value={lastName} type="text" required/>
-                        <br/>
-                        <input onChange={emailHandleChange} className="text-box" name="last-name" placeholder="Email" value={email} type="text" required/>
-                        <br/>
-                        <label>How much do you want to spend each month?</label>
-                        <br/>
-                        <input onChange={budgetHandleChange} className="text-box" name="last-name" placeholder="Budget" value={budget} type="number" required/>
-                        <br/>
-                        <input className="submit-button" type="submit" value="Submit"></input>
-                    </form>
-                    <h2>LOGIN IN</h2>
-                    <form className='new-user-form' onSubmit={handleLogin}>
-                        <input onChange={userHandleChange} className="text-box" name="email" placeholder="Email" value={blank} type="text" required/>
-                        <br/>
-                        <input className="submit-button" type="submit" value="LOGIN"></input>
-                    </form>
-                    <button onClick={handleClose}>Exit</button>
-                </div>
+                <Login handleClose={handleClose} currentUser={props.currentUser} setCurrentUser={props.setCurrentUser}/>
             ):(
                 <div></div>
             )}
