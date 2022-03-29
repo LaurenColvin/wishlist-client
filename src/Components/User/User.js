1ef48b4a22a6c1bfcc8473844023bea5735bc74a
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-29 10:46:48 -0700

    adding useEffect to fetch user data on page load

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,4 +1,13 @@
-const User = () => {
+import { useState, useEffect } from "react";
+
+const User = (props) => {
+
+    useEffect(() => {
+        fetch(props.urlBase + "/user/" + props.currentUser )
+          .then((response) => response.json())
+          .then((data) => console.log(data.user));
+      }, []);
+
     return (
         <div className='user-page'>
             <h1>Please login to see your wishlist and wardrobe</h1>
