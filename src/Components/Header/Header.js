a38567b9a32b01945509028f3ed77b53fed99ff5
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-30 19:20:03 -0700

    adding hamburger menu for mobile view

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,17 +2,30 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import {faUserCircle} from '@fortawesome/free-solid-svg-icons'
 import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
 
 const Header = (props) => {
 
+  const [menuOpen, setMenuOpen] = useState(true);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="header">
       <nav>
+        <FontAwesomeIcon onClick={toggleMenu} className='hamburger-menu' icon={faBars} size="2x" style={{color:"#FA5272"}}/>
+        <div className={menuOpen ? "nav-bar-closed" : "nav-bar-open"}>
+            <Link onClick={toggleMenu} className='link' to='/'><h3>home</h3></Link>
+            <Link onClick={toggleMenu} className='link' to='/my-wishlist'><h3>my wishlist</h3></Link>
+            <Link onClick={toggleMenu} className='link' to='/about'><h3>about</h3></Link>           
+        </div>
         <div className='nav-links'>
             <Link className='link' to='/'><h3>home</h3></Link>
-            <Link className='link' to='/category'><h3>category</h3></Link>
+            <Link className='link' to='/my-wishlist'><h3>my wishlist</h3></Link>
             <Link className='link' to='/about'><h3>about</h3></Link>           
         </div>
         <Link className='link' to='/'><h1>Wishlist</h1></Link>
